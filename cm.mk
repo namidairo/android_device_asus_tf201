06fbcd3f272947e6cd611b7e6db7cfff8293b3fd
Josh <lj50036@hotmail.com>
2013-12-27 13:29:05 -0600

    tf201: cm.mk changes

--- cm.mk
+++ cm.mk
@@ -1,3 +1,10 @@
+# Resolution values for bootanimation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 1200
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cmb/config/common_full_tablet_wifionly.mk)
+
 # Inherit device configuration for tf201.
 $(call inherit-product, device/asus/tf201/full_tf201.mk)
 
@@ -7,12 +14,13 @@ $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := cm_tf201
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := cmb_tf201
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := tf201
 PRODUCT_MODEL := EPAD
 PRODUCT_MANUFACTURER := asus
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=EeePad BUILD_FINGERPRINT=asus/US_epad/EeePad:4.1.1/JRO03C/US_epad-10.4.2.18-20121122:user/release-keys PRIVATE_BUILD_DESC="US_epad-user 4.1.1 JRO03C US_epad-10.4.2.18-20121122 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=EeePad BUILD_FINGERPRINT=asus/US_epad/EeePad:4.2.1/JOP40D/US_epad-10.6.1.14.10-20130801:user/release-keys PRIVATE_BUILD_DESC="US_epad-user 4.2.1 JOP40D US_epad-10.6.1.14.10-20130801 release-keys"
 
 # Release name and versioning
-PRODUCT_RELEASE_NAME := tf201
+PRODUCT_RELEASE_NAME := TF201
