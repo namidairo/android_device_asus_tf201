16366cd17a865b73ecf3ba1a82ebd8a1ae58dd0a
Richard Huynh <voxlympha@gmail.com>
2014-03-31 16:06:34 +1100

    TF201: Remove prebuilt kernel

--- device_tf201.mk
+++ device_tf201.mk
@@ -21,16 +21,8 @@ $(call inherit-product-if-exists, vendor/asus/tf201/tf201-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/asus/tf201/overlay
 
-# Prebuilt kernel location
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/asus/tf201/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
 # Files needed for boot/recovery image
 PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel \
     $(LOCAL_PATH)/ramdisk/init.cardhu.rc:root/init.cardhu.rc\
     $(LOCAL_PATH)/ramdisk/ueventd.cardhu.rc:root/ueventd.cardhu.rc \
     $(LOCAL_PATH)/ramdisk/init.cardhu.usb.rc:root/init.cardhu.usb.rc \
